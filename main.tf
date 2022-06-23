3684ee37de6c37e2ddecf6dbe1b55f331f7e0c85
Dustin Hall <halldk123@gmail.com>
2022-06-23 20:34:39 +0000

    Found all public IPs and applied to rules

--- main.tf
+++ main.tf
@@ -32,28 +32,28 @@ resource "aws_subnet" "hashicat" {
 
 resource "aws_security_group" "hashicat" {
   name = "${var.prefix}-security-group"
-
+  depends_on = []
   vpc_id = aws_vpc.hashicat.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${aws_eip.hashicat.public_ip}/32"]
+    cidr_blocks = local.public_ips
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["${aws_eip.hashicat.public_ip}/32"]
+    cidr_blocks = local.public_ips
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["${aws_eip.hashicat.public_ip}/32"]
+    cidr_blocks = local.public_ips
   }
 
   egress {

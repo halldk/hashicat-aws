623c32c68d48637fb9a299e0efd1e54d34c8cb76
Dustin Hall <halldk123@gmail.com>
2022-06-23 20:41:30 +0000

    Found IP

--- main.tf
+++ main.tf
@@ -39,21 +39,21 @@ resource "aws_security_group" "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = local.public_ips
+    cidr_blocks = ["44.206.205.196/32"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = local.public_ips
+    cidr_blocks = ["44.206.205.196/32"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = local.public_ips
+    cidr_blocks = ["44.206.205.196/32"]
   }
 
   egress {
